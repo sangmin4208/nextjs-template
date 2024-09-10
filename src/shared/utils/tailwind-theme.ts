4d41378a78cfedb068ec98619db9dd40d29d3ba4
이상민 <sangmin4208@gmail.com>
2024-09-10 10:39:50 +0900

    🐛 tailwind theme 수정

--- src/shared/utils/tailwind-theme.ts
+++ src/shared/utils/tailwind-theme.ts
@@ -1,4 +1,5 @@
+import resolveConfig from 'tailwindcss/resolveConfig'
 import config from '@/../tailwind.config'
 
-export const TailwindTheme = config.theme
-export const TailwindColor = TailwindTheme.extend.colors
+export const TailwindTheme = resolveConfig(config).theme
+export const TailwindColor = TailwindTheme.colors
